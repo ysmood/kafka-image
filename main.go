44b313e1536e847293282f9c250a01bcfeaaa048
Yad Smood <y.s.inside@gmail.com>
2019-06-07 18:00:40 +0900

    If zookeeper shutdown first, kafka will try to keep reconnecting it and ignore the terminal signal. After a while docker will force kill everything, the pid file won't be removed by kafka process, when restart kafka it will exit immediately because pid exists

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"os"
+	"syscall"
 
 	kit "github.com/ysmood/gokit"
 )
 
 func main() {
-	go kit.Exec("zookeeper-server-start.sh", "config/zookeeper.properties").MustDo()
+	zookeeper := kit.Exec("zookeeper-server-start.sh", "config/zookeeper.properties")
+	go zookeeper.Do()
 
 	host := os.Getenv("KAFKA_ADVERTISED_HOST_NAME")
 	if host == "" {
@@ -19,5 +21,14 @@ func main() {
 	confPath := "config/server.properties.env"
 	kit.OutputFile(confPath, conf+"\nadvertised.host.name="+host, nil)
 
-	kit.Exec("kafka-server-start.sh", confPath).MustDo()
+	kafka := kit.Exec("kafka-server-start.sh", confPath)
+	go kafka.Do()
+
+	kit.WaitSignal(syscall.SIGTERM)
+
+	kit.KillTree(kafka.GetCmd().Process.Pid)
+	kafka.GetCmd().Process.Wait()
+
+	kit.KillTree(zookeeper.GetCmd().Process.Pid)
+	zookeeper.GetCmd().Process.Wait()
 }
