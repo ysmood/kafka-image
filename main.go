43d01263b41a19679be732e9b59fb53ba327ee68
Yad Smood <y.s.inside@gmail.com>
2019-06-12 00:47:36 +0900

    simplify code

--- main.go
+++ main.go
@@ -11,18 +11,7 @@ func main() {
 	zookeeper := kit.Exec("zookeeper-server-start.sh", "config/zookeeper.properties")
 	go zookeeper.Do()
 
-	confPath := "config/server.properties"
-
-	host := os.Getenv("KAFKA_ADVERTISED_HOST_NAME")
-	if host != "" {
-		conf, err := kit.ReadStringFile("config/server.properties")
-		kit.E(err)
-
-		confPath += ".env"
-		kit.OutputFile(confPath, conf+"\nadvertised.host.name="+host, nil)
-	}
-
-	kafka := kit.Exec("kafka-server-start.sh", confPath)
+	kafka := kit.Exec("kafka-server-start.sh", configKafkaViaEnvs("config/server.properties"))
 	go kafka.Do()
 
 	kit.WaitSignal(syscall.SIGTERM)
@@ -38,4 +27,16 @@ func main() {
 
 	kit.KillTree(zookeeper.GetCmd().Process.Pid)
 	zookeeper.GetCmd().Process.Wait()
+}
+
+func configKafkaViaEnvs(path string) string {
+	host := os.Getenv("KAFKA_ADVERTISED_HOST_NAME")
+	if host != "" {
+		conf, err := kit.ReadStringFile("config/server.properties")
+		kit.E(err)
+
+		path += ".env"
+		kit.OutputFile(path, conf+"\nadvertised.host.name="+host, nil)
+	}
+	return path
 }
